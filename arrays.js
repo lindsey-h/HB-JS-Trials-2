9e4b8cfff2b011a8659d4277fff5d06a6cb73574
Lindsey Hight <lindseydhight@gmail.com>
2021-08-02 23:16:40 +0000

    Finished arrays.js

--- arrays.js
+++ arrays.js
@@ -3,17 +3,35 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+    const result = [];
+
+    for (const i in items) {
+        if (i%2 === 0) {
+            result.push(items[i]);
+        }
+    }
+
+    console.log(result)
+
 }
 
 
-// 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  let sortedItems = items.sort((a, b) => a - b);
+  sortedItems =sortedItems.slice(0, n);
+  sortedItems.reverse();
+
+  console.log(sortedItems);
+
+
+  
+  // s.slice(start, end)
 }
